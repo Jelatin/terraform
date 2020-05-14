e174436dec6a156f4d94c25d914960be1373f918
Varaporn Nilrattanabunpot <varaponi@ICTUS.COM>
2020-05-14 11:19:28 +0700

    update code

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_route_table" "public_route_table" {
   
   route {
     cidr_block = "172.30.0.0/16"
-    gateway_id = aws_vpn_gateway.vgw.id
+    gateway_id = var.gateway_id
   }
 
   tags = {
@@ -121,7 +121,7 @@ resource "aws_route_table" "private_route_table" {
 
   route {
     cidr_block      = "172.30.0.0/16"
-    gateway_id      = aws_vpn_gateway.vgw.id
+    gateway_id      = var.gateway_id
   }
 
   tags = {
